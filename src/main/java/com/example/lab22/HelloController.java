f7e6e6bbf6fb089596d3b79a026cc15bfc7fa5ff
wojtek123 <1889wojtek@gmail.com>
2022-05-08 23:43:41 +0200

    obliczenie ciezaru blachy stalowej

--- src/main/java/com/example/lab22/HelloController.java
+++ src/main/java/com/example/lab22/HelloController.java
@@ -28,9 +28,12 @@ public class HelloController {
         double pole_p = 2*3.14*d_promien*d_promien;
         double pole_c = pole_b + pole_p;
 
-        results.setText("");
-        String temp = Double.toString(pole_c);
-        results.appendText(temp);
+        double cięzar = 7.85d; //na m^2 przy 1mm grubosci
+        double ciezar_blachy = pole_c*d_grubosc;
+        String temp = Double.toString(ciezar_blachy);
+
+        String wynik = "Ciężar blachy stalowej o podanych wymiarach wynosić będzie: "+ temp+" kg.";
+        results.appendText(wynik);
 
 
     }
