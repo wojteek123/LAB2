02feeda92667f08c4f75ef69ed37b4803cfc1c97
wojtek123 <1889wojtek@gmail.com>
2022-05-09 00:15:36 +0200

    obliczenie ciezaru blachy stalowej

--- src/main/java/com/example/lab22/HelloController.java
+++ src/main/java/com/example/lab22/HelloController.java
@@ -11,30 +11,33 @@ public class HelloController {
     public TextField grubosc;
     public TextArea results;
 
+    public double truncate(double n){ //funkcja zwraca liczbe skrocona do 2 wartosci po przecinku
+        n = n*Math.pow(10,2);
+        n = Math.floor(n);
+        n = n / Math.pow(10,2);
+        return n;
+    }
+
     @FXML
     protected void oblicz () {
-        String s_promien =promien.getText();
-        String s_wysokosc =wysokosc.getText();
-        String s_grubosc =grubosc.getText();
+        results.setText(""); //wyczycic TextArea przed wyswietleniem
 
-
-
-        System.out.printf(s_grubosc);
-        double d_promien = Double.parseDouble(s_promien);
-        double d_wysokosc = Double.parseDouble(s_wysokosc);
-        double d_grubosc = Double.parseDouble(s_grubosc);
+        double d_promien = Double.parseDouble(promien.getText());
+        double d_wysokosc = Double.parseDouble(wysokosc.getText());
+        double d_grubosc = Double.parseDouble(grubosc.getText());
 
         double pole_b = 2*3.14*d_promien*d_wysokosc;
         double pole_p = 2*3.14*d_promien*d_promien;
-        double pole_c = pole_b + pole_p;
+        double pole_c = pole_b + pole_p; // mm^2
 
         double cięzar = 7.85d; //na m^2 przy 1mm grubosci
-        double ciezar_blachy = pole_c*d_grubosc;
-        String temp = Double.toString(ciezar_blachy);
+        double ciezar_blachy = (pole_c/10000)*d_grubosc;
 
-        String wynik = "Ciężar blachy stalowej o podanych wymiarach wynosić będzie: "+ temp+" kg.";
+
+        String temp = Double.toString(truncate(pole_c/10000));
+        String temp2 = Double.toString(truncate(ciezar_blachy));
+        String wynik = "Do wykonania walca, potrzeba będzie:"+ temp + " metrów kwadratowych blachy.\n Jej cięzar wynosi "+temp2+" kg.";
         results.appendText(wynik);
-
 
     }
 
